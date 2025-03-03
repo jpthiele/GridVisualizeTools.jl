51f3b60c16e0a85b59f208131c6b3bf9486645a3
Patrick Jaap <patrick.jaap@wias-berlin.de>
2025-03-03 12:00:16 +0100

    More user control over the types in marching_triangles

--- src/marching.jl
+++ src/marching.jl
@@ -268,6 +268,9 @@ Input:
     func: function on the grid nodes to be evaluated
     lines: vector of line definitions [a,b,c], s.t., ax + by + c = 0 defines a line
     levels: vector of levels for the iso-surface
+    Tc: scalar type of coordinates
+    Tp: vector type of coordinates
+    Tv: scalar type of function values
 
 Output:
     points: vector of 2D points of the intersections of the grid with the iso-surfaces or lines
@@ -277,15 +280,16 @@ Output:
 Note that passing both nonempty `lines` and `levels` will create a result with both types of points mixed.
 """
 function marching_triangles(
-        coord::Matrix{Tc},
+        coord::Matrix{T},
         cellnodes::Matrix{Ti},
         func,
         lines,
         levels;
-        Tv = Float32,
-        Tp = SVector{2, Tv}
-    ) where {Tc <: Number, Ti <: Number}
-    return marching_triangles([coord], [cellnodes], [func], lines, levels; Tv, Tp)
+        Tc = T,
+        Tp = SVector{2, Tc},
+        Tv = Float64
+    ) where {T <: Number, Ti <: Number}
+    return marching_triangles([coord], [cellnodes], [func], lines, levels; Tc, Tp, Tv)
 end
 
 
@@ -293,17 +297,18 @@ end
     $(SIGNATURES)
 
 
-Variant of `marching_tetrahedra` with multiple grid input
+Variant of `marching_triangles` with multiple grid input
 """
 function marching_triangles(
-        coords::Vector{Matrix{Tc}},
+        coords::Vector{Matrix{T}},
         cellnodes::Vector{Matrix{Ti}},
         funcs,
         lines,
         levels;
-        Tv = Float32,
-        Tp = SVector{2, Tv},
-    ) where {Tc <: Number, Ti <: Number}
+        Tc = T,
+        Tp = SVector{2, Tc},
+        Tv = Float64
+    ) where {T <: Number, Ti <: Number}
     points = Vector{Tp}(undef, 0)
     values = Vector{Tv}(undef, 0)
     adjacencies = Vector{SVector{2, Ti}}(undef, 0)
